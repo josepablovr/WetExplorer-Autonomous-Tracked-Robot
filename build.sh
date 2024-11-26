a6dd2fae6ee2fe0112a211d5224284262aa5d5eb
josepablovr <josepablovasro@gmail.com>
2024-11-26 15:09:02 +0100

    Docker Automatic Workspace Finder

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+IMAGE_NAME="wetexplorer"
 
 # Navigate to the ros2_ws directory
 cd docker || exit 1
 
 # Build the Docker image using the Dockerfile in the current directory
-sudo docker build -t wetexplorer .
+sudo docker build -t "${IMAGE_NAME}" .
 
 # Return to the original directory and run the container
 cd - || exit 1
--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Define workspace and Docker variables
+export WETEXPLORER_WS=$(pwd)
+CONTAINER_NAME="cont"
+IMAGE_NAME="wetexplorer"
+WORKSPACE_PATH="${WETEXPLORER_WS}/ros2_ws"
+
+
 # Check if a container named "cont" is already running
 if sudo docker ps --filter "name=cont" --format "{{.Names}}" | grep -q "^cont$"; then
     # If the container is running, execute bash in it
@@ -8,6 +15,8 @@ else
     # Otherwise, run a new container
     sudo docker run --rm --name cont -it --user ros --network=host --ipc=host \
         -v /tmp/.X11-unix:/tmp/.X11-unix:rw --env=DISPLAY \
-        -v /home/pablo/humble/WetExplorer-Autonomous-Tracked-Robot/ros2_ws:/ros2_ws \
-        -v /dev:/dev --device-cgroup-rule='c *:* rmw' wetexplorer /bin/bash
+        -v "${WORKSPACE_PATH}":/ros2_ws \
+        -v /dev:/dev --device-cgroup-rule='c *:* rmw' "${IMAGE_NAME}" /bin/bash
 fi
+
+
