15c77d7fa52c8a807eca2d7b5fb74565f85ea3f3
josepablovr <josepablo2106@gmail.com>
2024-11-26 06:10:23 -0600

    Pkg update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,9 +2,9 @@
 
 set -e 
 
-source /opt/ros/humble/setup.bash
-source /install/setup.bash
-
+#source /opt/ros/noetic/setup.bash
+#source /catkin_ws/devel/setup.bash
+#source /usr/share/gazebo-11/setup.bash
 echo "Provided arguments: $@"
 
 exec $@
