a9be971f9ea61528d1aad66dc6e3bc65cd084e19
josepablovr <josepablo2106@gmail.com>
2025-05-30 16:50:11 +0200

    Add Costmap Plugin: RIngs are saved as obstacles to avoid damaging them

---
+++ ros2_ws/src/wetexplorer/wetexplorer_navigation/src/ring_layer.cpp
@@ -0,0 +1,200 @@
+/*********************************************************************
+ *
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, 2013, Willow Garage, Inc.
+ *  Copyright (c) 2020, Samsung R&D Institute Russia
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Eitan Marder-Eppstein
+ *         David V. Lu!!
+ *         Alexey Merzlyakov
+ *
+ * Reference tutorial:
+ * https://navigation.ros.org/tutorials/docs/writing_new_costmap2d_plugin.html
+ *********************************************************************/
+#include "wetexplorer_navigation/ring_layer.hpp"
+
+#include "nav2_costmap_2d/costmap_math.hpp"
+#include "nav2_costmap_2d/footprint.hpp"
+#include "rclcpp/parameter_events_filter.hpp"
+
+using nav2_costmap_2d::LETHAL_OBSTACLE;
+using nav2_costmap_2d::INSCRIBED_INFLATED_OBSTACLE;
+using nav2_costmap_2d::NO_INFORMATION;
+
+namespace wetexplorer_navigation
+{
+
+RingLayer::RingLayer()
+: last_min_x_(-std::numeric_limits<float>::max()),
+  last_min_y_(-std::numeric_limits<float>::max()),
+  last_max_x_(std::numeric_limits<float>::max()),
+  last_max_y_(std::numeric_limits<float>::max())
+{
+}
+
+// This method is called at the end of plugin initialization.
+// It contains ROS parameter(s) declaration and initialization
+// of need_recalculation_ variable.
+void
+RingLayer::onInitialize()
+{
+  auto node = node_.lock(); 
+  declareParameter("enabled", rclcpp::ParameterValue(true));
+  node->get_parameter(name_ + "." + "enabled", enabled_);
+
+  need_recalculation_ = false;
+  current_ = true;
+  // Subscribe to rings
+  ring_sub_ = node->create_subscription<visualization_msgs::msg::MarkerArray>(
+    "/map_rings", 10,
+    std::bind(&RingLayer::ringsCallback, this, std::placeholders::_1));
+
+}
+
+// The method is called to ask the plugin: which area of costmap it needs to update.
+// Inside this method window bounds are re-calculated if need_recalculation_ is true
+// and updated independently on its value.
+void RingLayer::updateBounds(
+  double /*robot_x*/, double /*robot_y*/, double /*robot_yaw*/, double * min_x,
+  double * min_y, double * max_x, double * max_y)
+{
+  if (need_recalculation_) {
+    last_min_x_ = *min_x;
+    last_min_y_ = *min_y;
+    last_max_x_ = *max_x;
+    last_max_y_ = *max_y;
+    // For some reason when I make these -<double>::max() it does not
+    // work with Costmap2D::worldToMapEnforceBounds(), so I'm using
+    // -<float>::max() instead.
+    *min_x = -std::numeric_limits<float>::max();
+    *min_y = -std::numeric_limits<float>::max();
+    *max_x = std::numeric_limits<float>::max();
+    *max_y = std::numeric_limits<float>::max();
+    need_recalculation_ = false;
+  } else {
+    double tmp_min_x = last_min_x_;
+    double tmp_min_y = last_min_y_;
+    double tmp_max_x = last_max_x_;
+    double tmp_max_y = last_max_y_;
+    last_min_x_ = *min_x;
+    last_min_y_ = *min_y;
+    last_max_x_ = *max_x;
+    last_max_y_ = *max_y;
+    *min_x = std::min(tmp_min_x, *min_x);
+    *min_y = std::min(tmp_min_y, *min_y);
+    *max_x = std::max(tmp_max_x, *max_x);
+    *max_y = std::max(tmp_max_y, *max_y);
+  }
+}
+
+// The method is called when footprint was changed.
+// Here it just resets need_recalculation_ variable.
+
+
+void RingLayer::ringsCallback(
+  const visualization_msgs::msg::MarkerArray::SharedPtr msg)
+{
+  rings_.clear();
+  for (auto & m : msg->markers) {
+    double radius = 0.5 * m.scale.x;
+    rings_.emplace_back(m.pose.position, radius);
+  }
+  need_recalculation_ = true;
+}
+
+void RingLayer::onFootprintChanged()
+{
+  need_recalculation_ = true;
+
+}
+
+// The method is called when costmap recalculation is required.
+// It updates the costmap within its window bounds.
+// Inside this method the costmap gradient is generated and is writing directly
+// to the resulting costmap master_grid without any merging with previous layers.
+void RingLayer::updateCosts(
+  nav2_costmap_2d::Costmap2D & master_grid,
+  int min_i, int min_j,
+  int max_i, int max_j)
+{
+  if (!enabled_ || rings_.empty()) {
+    return;
+  }
+
+  auto * master_array = master_grid.getCharMap();
+  double res = master_grid.getResolution();
+
+   // Clamp window to map size
+  unsigned int size_x = master_grid.getSizeInCellsX();
+  unsigned int size_y = master_grid.getSizeInCellsY();
+  min_i = std::max(0, min_i);
+  min_j = std::max(0, min_j);
+  max_i = std::min<int>(size_x, max_i);
+  max_j = std::min<int>(size_y, max_j);
+
+  // Draw each ring into master_array and record obstacle cells
+  for (auto & r : rings_) {
+    const auto & center = r.first;
+    double rr = r.second;
+    unsigned int cx, cy;
+    if (!master_grid.worldToMap(center.x, center.y, cx, cy)) {
+      continue;
+    }
+    int radius_cells = std::ceil(rr / res);
+
+    for (int dy = -radius_cells; dy <= radius_cells; ++dy) {
+      int yy = int(cy) + dy;
+      if (yy < min_j || yy >= max_j) continue;
+      for (int dx = -radius_cells; dx <= radius_cells; ++dx) {
+        int xx = int(cx) + dx;
+        if (xx < min_i || xx >= max_i) continue;
+        if (dx*dx + dy*dy <= radius_cells*radius_cells) {
+          // mark obstacle in costmap
+          int idx = master_grid.getIndex(xx, yy);
+          master_array[idx] = LETHAL_OBSTACLE;
+
+        }
+      }
+    }
+  }
+
+
+}
+
+
+}  // namespace wetexplorer_navigation
+
+// This is the macro allowing a nav2_gradient_costmap_plugin::RingLayer class
+// to be registered in order to be dynamically loadable of base type nav2_costmap_2d::Layer.
+// Usually places in the end of cpp-file where the loadable class written.
+#include "pluginlib/class_list_macros.hpp"
+PLUGINLIB_EXPORT_CLASS(wetexplorer_navigation::RingLayer, nav2_costmap_2d::Layer)
